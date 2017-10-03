a5ac890b06a06c29b8a5d9b00b2c9ad750fc41d0
Glenn Gartner <glenngartner@gmail.com>
2017-10-02 22:57:50 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/starter01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/starter01.dir/main.cpp.o"
+  "starter01.pdb"
+  "starter01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/starter01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
