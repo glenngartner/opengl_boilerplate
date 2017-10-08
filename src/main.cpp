5b76c5c3b42c473c08c06bb8b6a68f9b19a226fe
Glenn Gartner <glenngartner@gmail.com>
2017-10-08 00:09:11 -0400

    add main and GL_Renderer.cpp back to src. removed during merge

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "GL_Renderer.h"
+
+
+int main() {
+    // launch the openGL visuals
+//    std::shared_ptr<GL_Renderer> renderer = std::make_shared<GL_Renderer>();
+    GL_Renderer *renderer = new GL_Renderer();
+    renderer->setup();
+    renderer->draw();
+
+    return 0;
+}
+
